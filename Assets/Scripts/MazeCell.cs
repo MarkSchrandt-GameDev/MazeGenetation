a59e055fa323faf1c1373a94f87d2e7cc06840b6
mark <mark.schrandt@hva.nl>
2025-05-19 19:30:58 +0200

    Add visual assets

---
+++ Assets/Scripts/MazeCell.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MazeCell : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _wallN;
+
+    [SerializeField]
+    private GameObject _wallE;
+
+    [SerializeField]
+    private GameObject _wallS;
+
+    [SerializeField]
+    private GameObject _wallW;
+
+    [SerializeField]
+    private GameObject _unvisitedCell;
+
+    public bool IsVisited { get; private set; }
+
+    public void Visit()
+    {
+        IsVisited = true;
+        _unvisitedCell.SetActive(false);
+    }
+
+    public void SetWall(int direction, bool state)
+    {
+        switch (direction)
+        {
+            case 0:
+                _wallN.SetActive(state);
+                break;
+            case 1:
+                _wallE.SetActive(state);
+                break;
+            case 2:
+                _wallS.SetActive(state);
+                break;
+            case 3:
+                _wallW.SetActive(state);
+                break;
+            default:
+                Debug.LogError("Invalid wall direction");
+                break;
+        }
+    }
+}
