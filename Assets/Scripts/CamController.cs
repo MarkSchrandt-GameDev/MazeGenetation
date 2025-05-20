d14c6700a3ad8f3b476aefb21b64caa4592cab43
mark <mark.schrandt@hva.nl>
2025-05-20 15:48:53 +0200

    Added camera controller

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class MazeCameraController : MonoBehaviour
+{
+    [Header("References")]
+    public MazeGenerator generator;
+
+    [Header("Camera Mode")]
+    public float padding = 1.2f;
+
+    private Camera cam;
+
+    private void Awake() => cam = GetComponent<Camera>();
+
+    private void OnEnable()
+    {
+        generator.onMazeGenerated += AdjustCamera;
+    }
+
+    private void OnDisable()
+    {
+        generator.onMazeGenerated -= AdjustCamera;
+    }
+
+    public void AdjustCamera()
+    {
+        int w = generator.Width;
+        int h = generator.Height;
+        float s = generator.CellSpacing;
+
+        float worldW = (w - 1) * s;
+        float worldH = (h - 1) * s;
+        Vector3 center = new Vector3(worldW, 0, worldH) * 0.5f;
+
+        cam.orthographic = true;
+        cam.orthographicSize = Mathf.Max(worldW, worldH) * 0.5f * padding;
+        transform.position = center + Vector3.up * (worldW + worldH) * 0.5f;
+        transform.rotation = Quaternion.Euler(90f, 0f, 0f); 
+    }
+}
