4bdd67c757f77d94589ac998fa6f8b52b7a17340
mark <mark.schrandt@hva.nl>
2025-05-20 14:30:48 +0200

    Added UI element

---
+++ Assets/Scripts/MazeController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MazeUIController : MonoBehaviour
+{
+    [Header("UI References")]
+    [SerializeField] private TMP_InputField _widthInput;
+    [SerializeField] private TMP_InputField _heightInput;
+    [SerializeField] private Slider _delaySlider;
+    [SerializeField] private TextMeshProUGUI _delayValueText;
+    [SerializeField] private Button _generateButton;
+    [SerializeField] private MazeGenerator _mazeGenerator;
+
+    private void Awake()
+    {
+        _widthInput.onEndEdit.AddListener(OnWidthChanged);
+        _heightInput.onEndEdit.AddListener(OnHeightChanged);
+        _delaySlider.onValueChanged.AddListener(OnDelayChanged);
+        _generateButton.onClick.AddListener(OnGenerateClicked);
+    }
+
+    private void Start()
+    {
+        // Initialize UI with current generator settings
+        _widthInput.text = _mazeGenerator.Width.ToString();
+        _heightInput.text = _mazeGenerator.Height.ToString();
+        _delaySlider.value = _mazeGenerator.GenerationDelay;
+        UpdateDelayText(_mazeGenerator.GenerationDelay);
+    }
+
+    private void OnGenerateClicked()
+    {
+       _mazeGenerator.GenerateMaze();
+    }
+
+    private void OnWidthChanged(string text)
+    {
+        if (int.TryParse(text, out int val))
+            _mazeGenerator.Width = val;
+    }
+
+    private void OnHeightChanged(string text)
+    {
+        if (int.TryParse(text, out int val))
+            _mazeGenerator.Height = val;
+    }
+
+    private void OnDelayChanged(float val)
+    {
+        _mazeGenerator.GenerationDelay = val;
+        UpdateDelayText(val);
+    }
+
+    private void UpdateDelayText(float val)
+    {
+        if (_delayValueText != null)
+            _delayValueText.text = val.ToString("F2") + "s";
+    }
+}
